448b6aa2064fff2a44e6cb782066b2ca8d7cb8df
neX- <hoanglongtran0698@gmail.com>
2019-04-25 17:48:53 +1000

    finish header (almost)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
 	validates(:name, presence: true, length: { maximum: 50, minimum: 4})
-	#VALID_EMAIL_REGEX = /\A\w+\.\w+@rmit\.edu\.au\z/i
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A\w+\.\w+@rmit\.edu\.au\z/i
 	validates:email, presence: true, length: { maximum: 255, minimum: 4}, 
 					format: { with: VALID_EMAIL_REGEX},
 					uniqueness: { case_sensitive: false}
 	has_secure_password
-	#VALID_PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
-	validates :password, presence: true, length: {minimum: 8}
+	VALID_PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
+	validates :password, presence: true, length: {minimum: 8},
+					format: {with: VALID_PASSWORD_REGEX}
 
 	# Returns the hash digest of the given string.
   	def User.digest(string)
