1134864d3967645bef0dd7eb9260a17232752fb8
GrimArbre <andohadi@gmail.com>
2019-05-02 06:56:39 +1000

    fixing html

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,9 +1,9 @@
 module CoursesHelper
 
- # def gravatar_for(course)
- #   gravatar_id = Digest::MD5::hexdigest(course.category.downcase)
- #   gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
- #   image_tag(gravatar_url, alt: course.name, class: "gravatar")
- # end
+ def gravatar_course(course)
+   gravatar_id = Digest::MD5::hexdigest("potato@example.com")
+   gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+   image_tag(gravatar_url, class: "gravatar")
+ end
 
 end
