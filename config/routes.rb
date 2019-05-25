05c743db255b88c80e3f8c5831edb03fc1adcaa9
neX- <hoanglongtran0698@gmail.com>
2019-05-25 22:08:16 +1000

    Contact us page layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contacts
   get 'vote/create'
   get 'vote/destroy'
   get 'locations/new'
@@ -10,11 +11,11 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   get  '/login',   to: 'sessions#new'
   post '/login',   to: 'sessions#create'
   delete '/logout',to: 'sessions#destroy'
+  resources :send_feedbacks, only: [:edit]
   resources :courses do
 	resources :votes, only: [:create, :destroy, :update]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_25_064019) do
+ActiveRecord::Schema.define(version: 2019_05_25_120520) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "message"
+    t.string "email"
   end
 
   create_table "courses", force: :cascade do |t|
