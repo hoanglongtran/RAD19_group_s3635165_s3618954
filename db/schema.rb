5b3fda95ca52662684b620b5a3f17aed360550c8
neX- <hoanglongtran0698@gmail.com>
2019-05-25 17:00:25 +1000

    Commit before merging

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_17_044349) do
+ActiveRecord::Schema.define(version: 2019_05_25_064019) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2019_05_17_044349) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
