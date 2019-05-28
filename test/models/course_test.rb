123959b6ae0170d6f28aadbf2b249916712727fa
GrimArbre <andohadi@gmail.com>
2019-05-28 23:29:01 +1000

    fixed course model tests

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -5,11 +5,9 @@ class CourseTest < ActiveSupport::TestCase
     @user = User.create(name: "asdf", email: "gfd.asd@rmit.edu.au", password: "Foob@ra4", password_confirmation: "Foob@ra4")
     @location = Location.create(location: "asdf")
     @category = Category.create(category: "ghhs")
-    @course = @user.courses.build(name: "This is a course")
+    @course = @user.courses.build(name: "This is a course", description: "This is a description that is supposed to be more than 30 characters", prerequisite: "This is another course")
     @course.locations << @location
     @category.courses << @course
-    @course.description = "This is a description that is supposed to be more than 30 characters"
-    @course.prerequisite = "This is a another course"
     @course.save
   end
 
