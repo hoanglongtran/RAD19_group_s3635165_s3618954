7826fc092b3a1f4d78b9a72633c2662149ef55b3
neX- <hoanglongtran0698@gmail.com>
2019-05-28 23:59:17 +1000

    Final commit

--- test/controllers/courses_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CoursesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get courses_new_url
-    assert_response :success
-  end
-
-end
--- test/controllers/locations_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class LocationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get locations_new_url
-    assert_response :success
-  end
-
-end
