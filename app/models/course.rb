330389031e3155f4931b69d15fd5ba580e91c669
GrimArbre <andohadi@gmail.com>
2019-05-28 18:59:55 +1000

    basic picture upload complete

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,8 @@ class Course < ApplicationRecord
   belongs_to :category
   has_and_belongs_to_many :locations
   has_many :votes, dependent: :destroy
-
+ 
+  mount_uploader :picture, PictureUploader
   validates :name,  presence: true, length: { maximum: 100 },
                     uniqueness: { case_sensitive: false }
   validates :description,  presence: true, length: { maximum: 200 }
